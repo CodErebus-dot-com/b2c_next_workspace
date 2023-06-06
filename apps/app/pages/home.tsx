a76d42bf3a070d3db956bc3a555445c5ed31f678
Varkichan Cheeramkuzhiyil <varcheer@publicisgroupe.net>
2023-06-06 16:55:12 +0530

    update image element

--- apps/app/pages/home.tsx
+++ apps/app/pages/home.tsx
@@ -1,21 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 import Logo from "../public/images/Genesis_logo.svg"
 import Illustration from "../public/images/OTP-Authentication-Security.svg"
-import Image from 'next/image'
-import { ColorModeScript, Box } from '@chakra-ui/react'
-import { Link } from '@chakra-ui/next-js'
-// import { Html, Head, Main, NextScript } from 'next/document'
+// import Image from 'next/image'
+import { ColorModeScript, Box, Flex, Image } from '@chakra-ui/react'
 import { theme } from './_app'
 
 const Home = () => {
   return (
     <>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <div id="signin_container">  
+      <Flex>  
         <Box bgGradient="linear(to-r, #0f3e83, #0078d4, #3079ed, #00abec)" h="100vh" w="100vw" data-tenant-branding-background-color="true">
-          <img data-tenant-branding-background="true" alt="sign-in sign-up illustration" />
+          {/* <img data-tenant-branding-background="true" alt="sign-in sign-up illustration" /> */}
+          <Image data-tenant-branding-background="true" alt="sign-in sign-up illustration" />
           {/* <Image src='https://b2c-next-workspace-2c6ji9kfv-coderebus-dot-com.vercel.app/_next/static/media/OTP-Authentication-Security.7bf3ec73.svg' alt="sign-in sign-up illustration" priority /> */}
         </Box>
-        <div className="panel" id="panel">
+        <Flex h="100%" w="60%" direction="column">
           <table className="panel_layout" role="presentation">
             <tbody>
               <tr className="panel_layout_row">
@@ -23,11 +23,8 @@ const Home = () => {
                 <td id="panel_center">
                   <div className="inner_container">
                     <div className="api_container normaltext">
-                      <img data-tenant-branding-logo="true" alt="Genesisx Logo" />
-                      <Link href='/about' color='blue.400' _hover={{ color: 'blue.500' }}>
-                        About
-                      </Link>
-                      {/* <Image src={Logo} alt="GenesisX logo" priority /> */}
+                      {/* <img data-tenant-branding-logo="true" alt="Genesisx Logo" /> */}
+                      <Image alt="GenesisX logo" />
                       <div id="api" role="main"></div>
                     </div>
                   </div>
@@ -35,9 +32,8 @@ const Home = () => {
               </tr>
             </tbody>
           </table>
-        </div>
-
-      </div>
+        </Flex>
+      </Flex>
     </>
   )
 }
