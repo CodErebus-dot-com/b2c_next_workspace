ba10cb839de4aaba8169365a14ee5990d6165460
Varkichan Cheeramkuzhiyil <varcheer@publicisgroupe.net>
2023-06-12 01:30:20 +0530

    create a CustomScript component

--- apps/test-app/src/app/components/Base/index.tsx
+++ apps/test-app/src/app/components/Base/index.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 /* eslint-disable @next/next/no-img-element */
-import { ColorModeScript, Box, Flex, Image } from '@chakra-ui/react'
+import { Box, Flex, Image } from '@chakra-ui/react'
 // import { theme } from '../../../pages/_app'
 import styles from './styles';
 import Api from "../Api";
 import DummyApi from '../DummyApi';
-import Script from 'next/script';
+import CustomScript from './script';
 
 const Base = () => {
   return (
@@ -40,54 +40,8 @@ const Base = () => {
         {styles}
       </style>
 
-      <Script id='my-script' strategy='beforeInteractive'>
-        {`
-          document.addEventListener("DOMContentLoaded", function() {
-            appendClassNamesFromDummyApiToApi();
-          });
 
-          function appendClassNamesFromDummyApiToApi() {
-            const apiWrapper = document.getElementById("api");
-            const dummyApiWrapper = document.getElementById("dummy-api");
-            
-            if (!apiWrapper || !dummyApiWrapper) {
-              console.error("Invalid IDs for API or DummyAPI");
-              return;
-            }
-            
-            applyClassNames(apiWrapper, dummyApiWrapper);
-            ${process.env['NEXT_PUBLIC_NODE_ENV'] === 'production' && `removeDummyApi(dummyApiWrapper);`}
-          }
-          
-          function applyClassNames(apiNode, dummyApiNode) {
-            if (
-              apiNode.nodeType === Node.ELEMENT_NODE &&
-              dummyApiNode.nodeType === Node.ELEMENT_NODE
-            ) {
-              const classNames = dummyApiNode.classList;
-              for (let i = 0; i < classNames.length; i++) {
-                apiNode.classList.add(classNames[i]);
-              }
-            }
-          
-            const apiChildNodes = apiNode.childNodes;
-            const dummyApiChildNodes = dummyApiNode.childNodes;
-          
-            if (apiChildNodes.length === dummyApiChildNodes.length) {
-              for (let i = 0; i < apiChildNodes.length; i++) {
-                applyClassNames(apiChildNodes[i], dummyApiChildNodes[i]);
-              }
-            }
-          }          
-          
-          function removeDummyApi(dummyApiWrapper) {      
-            if (dummyApiWrapper) {
-              dummyApiWrapper.remove();
-            }
-          }
-        `}
-      </Script>
-      Home
+      <CustomScript />
     </>
   )
 }
