50a6866e776ed94f3c44827b4bf6de446849b591
Varkichan Cheeramkuzhiyil <varcheer@publicisgroupe.net>
2023-07-18 16:35:41 +0530

    test form - 3

--- apps/app/components/CustomForm.tsx
+++ apps/app/components/CustomForm.tsx
@@ -9,14 +9,14 @@ const CustomForm = () => {
           Sign in with your email address
         </Heading>
       </Box>
-      <Box className="mb-1"> {/* .error pageLevel */}
-        <Text className="text-red-700"></Text>
+      <Box mb={1}> {/* .error pageLevel */}
+        <Text color="red.700"></Text>
       </Box>
       <Box> {/* .entry */}
         <Box mt={3}> {/* .entry-item */}
           <FormLabel>Email Address</FormLabel>
-          <Box className="mb-1"> {/* .error pageLevel */}
-            <Text className="text-red-700"></Text>
+          <Box mb={1}> {/* .error pageLevel */}
+            <Text color="red.700"></Text>
           </Box>
           <Input type="email" />
         </Box>
@@ -25,8 +25,8 @@ const CustomForm = () => {
             <FormLabel>Password</FormLabel>
             <Link color="blue.500">Forgot your password?</Link>
           </Box>
-          <Box className="mb-1"> {/* .error pageLevel */}
-            <Text className="text-red-700"></Text>
+          <Box mb={1}> {/* .error pageLevel */}
+            <Text color="red.700"></Text>
           </Box>
           <Input type="password" mt={2} />
         </Box>
