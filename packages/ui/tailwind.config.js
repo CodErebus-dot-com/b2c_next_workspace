70fb04f264cba26e66074466cbc3ea6802d60603
Varkichan Cheeramkuzhiyil <varcheer@publicisgroupe.net>
2023-07-19 18:54:11 +0530

    remove purge from tailwind

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
     extend: {},
