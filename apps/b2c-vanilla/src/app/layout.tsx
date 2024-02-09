e5e5d43e525c997980696c87a7e4181441179e21
Varkichan Cheeramkuzhiyil <varkisebi@gmail.com>
2024-02-10 01:09:26 +0530

    perf: add vercel speed-insights

--- apps/b2c-vanilla/src/app/layout.tsx
+++ apps/b2c-vanilla/src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Header } from "@components";
 import { AuthProvider } from "@providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
             {children}
           </AuthProvider>
         </ChakraProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
