90be27b55e0d71f335b94b22e8e16cd1b8c9f06d
Varkichan Cheeramkuzhiyil <varkisebi@gmail.com>
2024-02-21 11:15:23 +0530

    fix: app proptype

--- apps/app/src/pages/_app.tsx
+++ apps/app/src/pages/_app.tsx
@@ -3,7 +3,12 @@ import theme from '@src/theme'
 import '@styles/globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
+
+interface AppProps {
+  Component: React.FC
+  pageProps: any
+} // temp AppProps
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
